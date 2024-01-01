656d9a7bebcc3e294772286760bc6d6c2cd72662
Ameer <ameerapex496@gmail.com>
2024-01-01 07:17:04 +0100

    added logic changing classes between buttons

--- src/script.js
+++ src/script.js
@@ -1,5 +1,18 @@
 // selecting elements Header Buttons
 var sliderButtons = document.querySelectorAll("#slider-Buttons");
+var imagesWrapEl = document.getElementsByClassName("images-Wrap")[0];
 // gelobal variables
 // functions
+var changeImg = function (index, sliderBtn) {
+    sliderButtons.forEach(function (sliderBtn) {
+        sliderBtn.classList.remove("slider-Active");
+    });
+    sliderBtn.classList.add("slider-Active");
+};
 // events
+sliderButtons.forEach(function (sliderBtn, index) {
+    sliderBtn.addEventListener("click", function () {
+        console.log(index, sliderBtn);
+        changeImg(index, sliderBtn);
+    });
+});
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,20 @@
 // selecting elements Header Buttons
 const sliderButtons = document.querySelectorAll("#slider-Buttons");
-
+const imagesWrapEl = document.getElementsByClassName("images-Wrap")[0];
 // gelobal variables
 
 // functions
-
+const changeImg = (index, sliderBtn) => {
+  sliderButtons.forEach((sliderBtn) => {
+    sliderBtn.classList.remove("slider-Active");
+  });
+  sliderBtn.classList.add("slider-Active");
+};
 // events
+sliderButtons.forEach((sliderBtn, index) => {
+  sliderBtn.addEventListener("click", () => {
+    console.log(index, sliderBtn);
+
+    changeImg(index, sliderBtn);
+  });
+});
