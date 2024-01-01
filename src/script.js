745d1d5df0b25fcbd4d558623611ae3ab5a73aa8
Ameer <ameerapex496@gmail.com>
2024-01-01 07:33:01 +0100

    added kiguc if start count

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 var sliderButtons = document.querySelectorAll("#slider-Buttons");
 var imagesHolderEl = document.getElementsByClassName("images-Holder")[0];
 // gelobal variables
+var numCounter = 0;
 // functions
 var changeImg = function (index, sliderBtn) {
     sliderButtons.forEach(function (sliderBtn) {
@@ -9,6 +10,18 @@ var changeImg = function (index, sliderBtn) {
     });
     sliderBtn.classList.add("slider-Active");
     imagesHolderEl.style.transform = "translateX(-".concat(index, "00%)");
+    startCounting();
+};
+// start counting function
+var startCounting = function () {
+    setInterval(function () {
+        numCounter++;
+        console.log(numCounter);
+        if (numCounter > 4) {
+            console.log(numCounter);
+            numCounter = 0;
+        }
+    }, 1000);
 };
 // events
 sliderButtons.forEach(function (sliderBtn, index) {
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,9 @@
 // selecting elements Header Buttons
 const sliderButtons = document.querySelectorAll("#slider-Buttons");
 const imagesHolderEl: any = document.getElementsByClassName("images-Holder")[0];
-// gelobal variables
 
+// gelobal variables
+let numCounter = 0;
 // functions
 const changeImg = (index, sliderBtn) => {
   sliderButtons.forEach((sliderBtn) => {
@@ -11,6 +12,20 @@ const changeImg = (index, sliderBtn) => {
 
   sliderBtn.classList.add("slider-Active");
   imagesHolderEl.style.transform = `translateX(-${index}00%)`;
+
+  startCounting();
+};
+
+// start counting function
+const startCounting = () => {
+  setInterval(() => {
+    numCounter++;
+    console.log(numCounter);
+    if (numCounter > 4) {
+      console.log(numCounter);
+      numCounter = 0;
+    }
+  }, 1000);
 };
 // events
 sliderButtons.forEach((sliderBtn, index) => {
